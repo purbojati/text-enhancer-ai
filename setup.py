76ed0b38db9f833e841d0677a1b7bc51362231c6
purbojati <official.purbojati@gmail.com>
2024-11-02 02:33:47 +0700

    Update setup

--- setup.py
+++ setup.py
@@ -13,10 +13,27 @@ OPTIONS = {
         'CFBundleShortVersionString': '1.0.0',
         'LSUIElement': True,
         'NSAppleEventsUsageDescription': 'This app requires access to automate keyboard input for text enhancement.',
+        'NSHighResolutionCapable': True,
     },
-    'packages': ['rumps', 'openai', 'pyperclip'],
-    'includes': ['packaging', 'packaging.version', 'packaging.specifiers', 'packaging.requirements'],
-    'excludes': ['tkinter'],
+    'packages': [
+        'rumps',
+        'openai',
+        'pyperclip',
+        'certifi',
+        'charset_normalizer',
+        'urllib3',
+        'requests'
+    ],
+    'includes': [
+        'packaging',
+        'packaging.version',
+        'packaging.specifiers',
+        'packaging.requirements',
+        'json',
+        'os',
+        'sys'
+    ],
+    'excludes': ['tkinter', 'PyQt5', 'wx', 'PySide2'],
 }
 
 setup(
@@ -25,4 +42,10 @@ setup(
     data_files=DATA_FILES,
     options={'py2app': OPTIONS},
     setup_requires=['py2app'],
+    install_requires=[
+        'rumps',
+        'openai',
+        'pyperclip',
+        'requests'
+    ],
 )
